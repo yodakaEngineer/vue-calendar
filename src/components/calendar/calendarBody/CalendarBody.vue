0e4435cf4572cebd0aa0584005d5cef8b33e7b50
yodaka <engineeryodaka@gmail.com>
2019-04-15 17:11:54 +0900

    単一ファイルコンポーネント化

---
+++ src/components/calendar/calendarBody/CalendarBody.vue
@@ -0,0 +1,19 @@
+<template>
+    <tbody>
+        <dateRow v-for="i in 6" v-bind:key="i" v-bind:index="i" :displayDate="displayDate" :displayMonth="displayMonth"></dateRow>
+    </tbody>
+</template>
+
+<script>
+    import DateRow from './DateRow.vue'
+
+    export default {
+        props: {
+            displayDate: Date,
+            displayMonth: Number
+        },
+        components: {
+            DateRow,
+        }
+    }
+</script>
